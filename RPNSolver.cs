79cf46128d45afb120c6e4ee8361fcb71d520d87
Brainless-Loveless-Me <118484482+Brainless-Loveless-Me@users.noreply.github.com>
2022-11-22 11:48:08 -0500

    Add files via upload

---
+++ RPNSolver.cs
@@ -0,0 +1,55 @@
+/*
+authour paul
+date11/22/22
+*/
+
+class RPNSolver
+{
+    public static void Main()
+    {
+        string equation = "6 3 4 + 5 * + 7 -";
+        string [] parts = equation.Split(' ');
+        
+        IntStack stack = new IntStack();
+
+        foreach (string ep in parts)
+        {
+            int tempNum = 0;
+            bool isDigit = Int32.TryParse(ep, out tempNum);
+            if (isDigit)
+            {
+                stack.Push(tempNum);
+            }
+            else
+            {
+                int? num1 = stack.Pop();
+                int? num2 = stack.Pop();
+                if (ep == "+")
+                {
+                    int? sum = num1 + num2;
+                    // if (sum == null) sum = 0;
+                    stack.Push((int)sum);
+                }
+                else if (ep == "-")
+                {
+                    int? diff = num1 - num2;
+                    // if (sum == null) sum = 0;
+                    stack.Push((int)diff);
+                }
+                else if (ep == "*")
+                {
+                    int? prod = num1 * num2;
+                    // if (sum == null) sum = 0;
+                    stack.Push((int)prod);
+                }
+                else if (ep == "/")
+                {
+                    int? quot = num1 / num2;
+                    // if (sum == null) sum = 0;
+                    stack.Push((int)quot);
+                }
+            }
+        }
+        Console.WriteLine(stack.Pop());
+    }
+}
